fcd02dc2b90b0a27634b61b37cd977acc275ccaa
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-08-12 23:13:06 +0000

    Update aims.py

--- aims.py
+++ aims.py
@@ -1,7 +1,7 @@
 import cv2
 # now i create a new cam object
 cap=cv2.VideoCapture(0)
-print("well imported......")
+print("well imported.....")
 #we will start by trying the eyeglass HaarCascade classifier
 "initializing the face classifier"
 face=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
