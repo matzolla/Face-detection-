b196b23c4425b3fd195e0fcdeb26eff05af1da56
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2023-09-05 08:00:11 +0000

    Update aims.py

--- aims.py
+++ aims.py
@@ -2,21 +2,21 @@ import cv2
 # now i create a new cam object
 cap=cv2.VideoCapture(0)
 print("well imported.....")
-#we will start by trying the eyeglass HaarCascade classifier
+# we will start by trying the eyeglass HaarCascade classifier
 "initializing the face classifier"
 face=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 while True:
-    #read the image from the cam
+    # read the image from the cam
     _,image=cap.read()
-    #now i convert the image into grayscale
+    # now i convert the image into grayscale
     image_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    #detect all faces in the image
+    # detect all faces in the image
     faces=face.detectMultiScale(image_gray,1.3,5)
-    #for every face draw a rectangle
+    # for every face draw a rectangle
     for x,y, width,height in glasses:
         cv2.rectangle(image,(x,y),(x+width,y+height),color=(255,0,0),thickness=1)
-        #cv2.putText(image,'Face',(10,500),font,1,(0,0,255),2)
+    # cv2.putText(image,'Face',(10,500),font,1,(0,0,255),2)
     cv2.imshow('Stark',image)
     if cv2.waitKey(1)==ord('q'):
         break
